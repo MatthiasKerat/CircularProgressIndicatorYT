f1ccef09eca4d33256a722a42c377f3c7079c166
Matthias Kerat <MatthiasKerat@web.de>
2022-10-06 23:26:59 +0200

    Initial commit

---
+++ app/src/main/java/com/kapps/circularprogressindicatoryt/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.kapps.circularprogressindicatoryt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kapps.circularprogressindicatoryt.ui.theme.CircularProgressIndicatorYTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CircularProgressIndicatorYTTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CircularProgressIndicatorYTTheme {
+        Greeting("Android")
+    }
+}
